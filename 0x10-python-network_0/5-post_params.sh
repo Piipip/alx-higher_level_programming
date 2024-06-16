7152b88253ef738d2182801d5d20605f9d58890f
Piipip <philipkorans@yahoo.com>
2024-06-15 18:35:37 -0700

    ot fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash scripts that sends a POST request to a given URL.
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L"
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L"
